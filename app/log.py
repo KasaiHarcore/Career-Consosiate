3db5c3cafb3a7ca4a11ba25ec62d26a9a84d9c3c
Thanh Dat <114926996+KasaiHarcore@users.noreply.github.com>
2024-10-14 11:52:58 +0700

    Update log

--- app/log.py
+++ app/log.py
@@ -9,21 +9,11 @@ from rich.panel import Panel
 from rich.prompt import Prompt
 
 welcome_message_chat = """
-# Welcome to Codoc, this is the Mixture-of-Agents (MoA) test demo!
-
-Mixture of Agents (MoA) is a novel approach that leverages the collective strengths of multiple LLMs to enhance performance, achieving state-of-the-art results. By employing a layered architecture where each layer comprises several LLM agents, MoA significantly outperforms GPT-4 Omni’s 57.5% on AlpacaEval 2.0 with a score of 65.1%, using only open-source models!
-
-This demo uses the following LLMs as reference models, then passes the results to the aggregate model for the final response:
-- groq/llama3-8b-8192
-- groq/llama3-70b-8192
-- groq/mixtral-8x7b-32768
-- groq/gemma-7b-it
+Welcome to the C&C, this is a demo for AI in HR.
 """
-
 
 def terminal_width():
     return get_terminal_size().columns
-
 
 WIDTH = min(120, terminal_width() - 10)
 
@@ -44,69 +34,14 @@ def print_banner(msg: str) -> None:
     console.print()
     console.print(banner, style="bold")
     console.print()
-
-
-def replace_html_tags(content: str):
-    """
-    Helper method to process the content before printing to markdown.
-    """
-    replace_dict = {
-        "<file>": "[file]",
-        "<class>": "[class]",
-        "<func>": "[func]",
-        "<method>": "[method]",
-        "<code>": "[code]",
-        "<original>": "[original]",
-        "<patched>": "[patched]",
-        "</file>": "[/file]",
-        "</class>": "[/class]",
-        "</func>": "[/func]",
-        "</method>": "[/method]",
-        "</code>": "[/code]",
-        "</original>": "[/original]",
-        "</patched>": "[/patched]",
-    }
-    for key, value in replace_dict.items():
-        content = content.replace(key, value)
-    return content
-
-
-def print_px(
-    msg: str, desc = "", print_callback: Callable[[dict], None] | None = None
-) -> None:
-    if not print_stdout:
-        return
-
-    msg = replace_html_tags(msg)
-    markdown = Markdown(msg)
-
-    name = "Codoc"
-    if desc:
-        title = f"{name} ({desc})"
-    else:
-        title = name
-
-    panel = Panel(
-        markdown,
-        title=title,
-        title_align="left",
-        border_style="magenta",
-        width=WIDTH,
-    )
-    console.print(panel)
-
-    if print_callback:
-        print_callback(
-            {"title": f"{name} ({desc})", "message": msg, "category": "Codoc"}
-        )
         
-def print_chat(
+def print_user(
     msg: str, desc = "", print_callback: Callable[[dict], None] | None = None
 ) -> None:
     if not print_stdout:
         return
 
-    name = "Chat Agent"
+    name = "Request"
     if desc:
         title = f"{name} ({desc})"
     else:
@@ -123,20 +58,19 @@ def print_chat(
 
     if print_callback:
         print_callback(
-            {"title": f"{name} ({desc})", "message": msg, "category": "chat_agent"}
+            {"title": f"{name} ({desc})", "message": msg, "category": "user"}
         
 )
         
-def print_user(
+def print_output(
     msg: str, desc = "", print_callback: Callable[[dict], None] | None = None
 ) -> None:
     if not print_stdout:
         return
 
-    msg = replace_html_tags(msg)
     markdown = Markdown(msg)
 
-    name = "User"
+    name = "Output"
     if desc:
         title = f"{name} ({desc})"
     else:
@@ -144,29 +78,28 @@ def print_user(
 
     panel = Panel(
         markdown,
-        title=title,
-        title_align="left",
-        border_style="cyan",
+        title = title,
+        title_align = "left",
+        border_style = "cyan",
         width=WIDTH,
     )
     console.print(panel)
 
     if print_callback:
         print_callback(
-            {"title": f"{name} ({desc})", "message": msg, "category": "user"}
+            {"title": f"{name} ({desc})", "message": msg, "category": "output"}
         )
 
 
-def print_retrieval(
+def print_llm(
     msg: str, desc="", print_callback: Callable[[dict], None] | None = None
 ) -> None:
     if not print_stdout:
         return
 
-    msg = replace_html_tags(msg)
     markdown = Markdown(msg)
 
-    name = "Context Retrieval Agent"
+    name = "Agent"
     if desc:
         title = f"{name} ({desc})"
     else:
@@ -175,8 +108,8 @@ def print_retrieval(
     panel = Panel(
         markdown,
         title=title,
-        title_align="left",
-        border_style="blue",
+        title_align = "left",
+        border_style = "blue",
         width=WIDTH,
     )
     console.print(panel)
@@ -185,57 +118,9 @@ def print_retrieval(
             {
                 "title": f"{name} ({desc})",
                 "message": msg,
-                "category": "context_retrieval_agent",
+                "category": "LLM",
             }
         )
-
-
-def print_doc_generation(
-    msg: str, desc="", print_callback: Callable[[dict], None] | None = None
-) -> None:
-    if not print_stdout:
-        return
-
-    msg = replace_html_tags(msg)
-    markdown = Markdown(msg)
-
-    name = "Documents Generation"
-    if desc:
-        title = f"{name} ({desc})"
-    else:
-        title = name
-
-    panel = Panel(
-        markdown,
-        title=title,
-        title_align="left",
-        border_style="yellow",
-        width=WIDTH,
-    )
-    console.print(panel)
-    if print_callback:
-        print_callback(
-            {
-                "title": f"{name} ({desc})",
-                "message": msg,
-                "category": "doc_generation",
-            }
-        )
-
-
-def print_description(content: str) -> None:
-    if not print_stdout:
-        return
-
-    title = "Project description"
-    panel = Panel(
-        content,
-        title=title,
-        title_align="left",
-        border_style="red",
-        width=WIDTH,
-    )
-    console.print(panel)
 
 
 def log_and_print(msg):
